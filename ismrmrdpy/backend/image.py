4caa68641fdfe3c085677b29face2bb5c64adc99
Ghislain Antony Vaillant <ghisvail@gmail.com>
2015-04-18 17:19:43 +0100

    add low-level api for image

---
+++ ismrmrdpy/backend/image.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2014-2015, Ghislain Antony Vaillant
+# All rights reserved.
+#
+# This file is distributed under the BSD License, see the LICENSE file or
+# checkout the license terms at http://opensource.org/licenses/BSD-2-Clause).
+
+from __future__ import absolute_import, division, print_function
+
+
+def make_header(*args, **kwargs):
+    pass
+
+def make_dtype(header):
+    pass
+
+def make_object(header, *args, **kwargs):
+    pass
+
+def frombytes(bytestring):
+    pass
+
+def set_flags(header, flags=None):
+    pass
+
+def clear_flags(header, flags=None):
+    pass
+
+def is_flag_set(header, flag):
+    pass
+
+def _verify_flags(flags):
+    pass
