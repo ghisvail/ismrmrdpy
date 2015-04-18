882d235019718a70c88c4d616c3c17794eb84253
Ghislain Antony Vaillant <ghisvail@gmail.com>
2015-04-18 16:22:19 +0100

    add backend submodule

---
+++ ismrmrdpy/backend/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2014-2015, Ghislain Antony Vaillant
+# All rights reserved.
+#
+# This file is distributed under the BSD License, see the LICENSE file or
+# checkout the license terms at http://opensource.org/licenses/BSD-2-Clause).
+
+from __future__ import absolute_import, division, print_function
