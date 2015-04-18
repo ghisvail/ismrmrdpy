8aa9cd3c8c6e5d5ce232017a16d25b2a1103ba28
Ghislain Antony Vaillant <ghisvail@gmail.com>
2015-04-18 17:19:25 +0100

    add low-level api for acquisition

---
+++ ismrmrdpy/backend/acquisition.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2014-2015, Ghislain Antony Vaillant
+# All rights reserved.
+#
+# This file is distributed under the BSD License, see the LICENSE file or
+# checkout the license terms at http://opensource.org/licenses/BSD-2-Clause).
+
+from __future__ import absolute_import, division, print_function
+
+
+def make_header(*args, **kwargs):
+    pass
+
+def make_dtype(header):
+    pass
+
+def make_object(header, *args, **kwargs):
+    pass
+
+def frombytes(bytestring):
+    pass
+
+def set_flags(header, flags=None):
+    pass
+
+def clear_flags(header, flags=None):
+    pass
+
+def is_flag_set(header, flag):
+    pass
+
+def _verify_flags(flags):
+    pass
+
+def set_channels(header, channels=None):
+    pass
+
+def clear_channels(header, channels=None):
+    pass
+
+def is_channel_set(header, channel):
+    pass
+
+def _verify_channels(flags):
+    pass    
