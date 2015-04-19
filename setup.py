a2d28d597d2361511332bbed303230421d7aa8a4
Ghislain Antony Vaillant <ghisvail@gmail.com>
2015-04-19 11:30:28 +0100

    add command for running the test suite

--- setup.py
+++ setup.py
@@ -10,6 +10,27 @@
 from __future__ import absolute_import, division, print_function
 
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class IsmrmrdpyTestCommand(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(
@@ -29,4 +50,6 @@ setup(
         "Topic :: Scientific/Engineering",
         ],
     packages=find_packages(exclude=['tests',]),
+    tests_require=['pytest'],
+    cmdclass = {'test': IsmrmrdpyTestCommand},
 )
