642cd34041a579fa37ea3790143d79842c7141f3
Ghislain Antony Vaillant <ghisvail@gmail.com>
2015-04-19 11:46:34 +0100

    add implementation for all makers

--- ismrmrdpy/backend/acquisition.py
+++ ismrmrdpy/backend/acquisition.py
@@ -8,17 +8,43 @@
 
 from __future__ import absolute_import, division, print_function
 
-from .constants import Constants, AcquisitionFlags, acquisition_header_dtype
+from .constants import Constants, AcquisitionFlags, DataTypes
+from .constants import acquisition_header_dtype, ismrmrd_to_numpy_dtypes
+import numpy
 
 
-def make_header(*args, **kwargs):
-    pass
+def make_header(version=Constants.version, *args, **kwargs):
+    header = numpy.zeros((), dtype=acquisition_header_dtype)
+    header['version'] = version
+    for key in kwargs:
+        if key in acquisition_header_dtype.fields:
+            header[key] = kwargs[key]
+    return header    
 
 def make_dtype(header):
-    pass
+    data_dtype = ismrmrd_to_numpy_dtypes[DataTypes.cxfloat]
+    data_shape = (header['active_channels'],
+                  header['number_of_samples'])
+    traj_dtype = ismrmrd_to_numpy_dtypes[DataTypes.float]
+    traj_shape = (header['number_of_samples'],
+                  header['trajectory_dimensions'])
+    return numpy.dtype([
+        ('head',    acquisition_header_dtype),
+        ('traj',    (traj_dtype, traj_shape)),
+        ('data',    (data_dtype, data_shape)),
+    ])
 
 def make_array(header=None, *args, **kwargs):
-    pass
+    header = header or make_header(**kwargs)
+    trajectory = None
+    data = None
+    dtype = make_dtype(header)
+    array = numpy.zeros((), dtype=dtype)
+    array['head'] = header
+    if trajectory is not None:
+        array['traj'] = trajectory
+    if data is not None:
+        array['data'] = data
 
 def frombytes(bytestring):
     pass
